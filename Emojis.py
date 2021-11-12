59fa38587445d5b1d5b4ef1291d213a5ba3c7d9b
DevendarDev <66067336+Musalappagaridevendrareddy@users.noreply.github.com>
2021-11-12 22:31:18 +0530

    Create Emojis.py

---
+++ Emojis.py
@@ -0,0 +1,14 @@
+#  Display emojis using python
+
+print("\U0001F917")
+print("\U0001F918")
+print("\U0001F637")
+print("\U0001F618")
+print("\U0001F621")
+print("\U0001F622")
+print("\U0001F624")
+print("\U0001F625")
+print("\U0001F626")
+
+# You can try with different numbers to get different emojis
+# The ouput you are viewing is not good in pycharm but try in other editor you will get better results
